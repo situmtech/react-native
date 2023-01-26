186dc4be3562cd98b5b4f551f7d382528e9209bd
Cristian Mayán <cristian.mayan@situm.com>
2023-01-26 17:59:32 +0100

    Added examples and native SDK version upgraded to 2.83.5

---
+++ example/src/examples/DrawRouteBetweenPOIs.tsx
@@ -0,0 +1,138 @@
+import React, {useEffect, useState} from 'react';
+import {Platform, View} from 'react-native';
+import MapView, {
+  Marker,
+  Overlay,
+  Polyline,
+  PROVIDER_GOOGLE,
+} from 'react-native-maps';
+import SitumPlugin from 'react-native-situm-plugin';
+import {SITUM_BUILDING_ID, SITUM_FLOOR_LEVEL} from '../situm';
+import {calculateBuildingLocation} from './Utils/CalculateBuildingLocation';
+import {fetchBuilding, fetchBuildingInfo} from './Utils/CommonFetchs';
+const LATITUDE_DELTA = 0.001;
+const LONGITUDE_DELTA = 0.001;
+
+const fetchPOIsFromBuilding = (building: any) => {
+  return new Promise((resolve, reject) => {
+    SitumPlugin.fetchIndoorPOIsFromBuilding(
+      building,
+      (indoorPOIs: any) => {
+        console.log(indoorPOIs);
+        resolve(indoorPOIs);
+      },
+      (error: any) => {
+        reject(error);
+      },
+    );
+  });
+};
+
+const requestDirections = (building: any, pois: any) => {
+  return new Promise((resolve, reject) => {
+    SitumPlugin.requestDirections(
+      [building, pois[0], pois[1]],
+      (route: any) => {
+        let latlngs = [];
+        for (let segment of route.segments) {
+          for (let point of segment.points) {
+            latlngs.push({
+              latitude: point.coordinate.latitude,
+              longitude: point.coordinate.longitude,
+            });
+          }
+        }
+        resolve(latlngs);
+      },
+      (error: any) => {
+        reject(error);
+      },
+    );
+  });
+};
+
+export const DrawRouteBetweenPOIs = () => {
+  const [building, setBuilding] = useState<any>();
+  const [mapImage, setMapImage] = useState<any>();
+  const [bounds, setBounds] = useState<any>();
+  const [bearing, setBearing] = useState<any>(0);
+  const [mapRegion, setMapRegion] = useState<any>();
+  const [pois, setPois] = useState<any>();
+  const [polylineLatlng, setPolylineLatlng] = useState<any>([]);
+
+  // useEffects to get data
+  useEffect(() => {
+    fetchBuilding(SITUM_BUILDING_ID)
+      .then(data => {
+        setBuilding(data);
+      })
+      .catch(err => console.log);
+  }, []);
+
+  useEffect(() => {
+    if (!building) return;
+    fetchPOIsFromBuilding(building)
+      .then(data => {
+        setPois(data);
+      })
+      .catch(err => console.log);
+  }, [building]);
+
+  useEffect(() => {
+    if (!building) return;
+    fetchBuildingInfo(building)
+      .then(data => {
+        const {bearing, bounds, map_region} = calculateBuildingLocation(
+          data.building,
+        );
+        setBearing(bearing);
+        setBounds(bounds);
+        setMapRegion(map_region);
+        if (data?.floors.length == 0) return;
+        var selectedFloor = data.floors.filter(
+          f => f.level == SITUM_FLOOR_LEVEL,
+        )[0];
+        setMapImage(selectedFloor.mapUrl);
+      })
+      .catch(err => console.log);
+  }, [building]);
+
+  //ask for directions once we know the building and the 2 POIS we use as origin and destination
+  useEffect(() => {
+    if (!building || !pois) return;
+    requestDirections(building, pois).then(res => setPolylineLatlng(res));
+  }, [building, pois]);
+
+  return (
+    <View>
+      <MapView
+        style={{width: '100%', height: '100%'}}
+        region={mapRegion}
+        provider={PROVIDER_GOOGLE}>
+        {mapImage && bounds && (
+          <Overlay image={{uri: mapImage}} bounds={bounds} bearing={bearing} />
+        )}
+        {
+          //RENDER MARKERS
+        }
+        {pois != null && <Marker coordinate={pois[0].coordinate} />}
+        {pois != null && <Marker coordinate={pois[1].coordinate} />}
+
+        {
+          //RENDER ROUTE
+        }
+        {polylineLatlng.length > 0 && (
+          <Polyline
+            key="editingPolyline"
+            coordinates={polylineLatlng}
+            strokeColor="#CF3B27"
+            zIndex={1001}
+            fillColor="#CF3B27"
+            strokeWidth={Platform.OS === 'ios' ? 7 : 9}
+            lineDashPattern={Platform.OS === 'ios' ? [1, 1] : [1, 15]}
+          />
+        )}
+      </MapView>
+    </View>
+  );
+};
---
+++ example/src/examples/RouteBetweenPOIs.tsx
@@ -0,0 +1,59 @@
+import React, {useEffect, useState} from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import SitumPlugin from 'react-native-situm-plugin';
+import {SITUM_BUILDING_ID} from '../situm';
+import styles from './styles/styles';
+import {fetchBuilding} from './Utils/CommonFetchs';
+
+export const RouteBetweenPOIs = () => {
+  const [building, setBuilding] = useState<any>();
+  const [indoorPOIs, setIndoorPOIs] = useState<any>();
+  const [route, setRoute] = useState<any>();
+
+  const populatePOIsFromBuilding = () => {
+    SitumPlugin.fetchIndoorPOIsFromBuilding(
+      building,
+      (indoorPOIs: any) => {
+        setIndoorPOIs(indoorPOIs);
+      },
+      (error: any) => {
+        console.log(error);
+      },
+    );
+  };
+
+  const requestDirections = () => {
+    //check if we have 2 pois at least
+    if (indoorPOIs.length >= 2)
+      SitumPlugin.requestDirections(
+        [building, indoorPOIs[0], indoorPOIs[1]],
+        (route: any) => {
+          setRoute(JSON.stringify(route));
+        },
+        (error: any) => {
+          console.log(error);
+        },
+      );
+  };
+
+  useEffect(() => {
+    // first load the building
+    fetchBuilding(SITUM_BUILDING_ID).then(setBuilding);
+  }, []);
+
+  useEffect(() => {
+    // later get pois
+    building && populatePOIsFromBuilding();
+  }, [building]);
+
+  useEffect(() => {
+    //finaly ask for directions
+    building && indoorPOIs && requestDirections();
+  }, [building, indoorPOIs]);
+
+  return (
+    <ScrollView>
+      <Text style={styles.text}>{route}</Text>
+    </ScrollView>
+  );
+};
