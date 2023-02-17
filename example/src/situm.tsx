695f02df31ba8442b2ed132123a43d6d3362b914
Cristian Mayán <cristian.mayan@situm.com>
2023-02-17 16:27:46 +0100

    Refactor example/README.md

--- example/src/situm.tsx
+++ example/src/situm.tsx
@@ -1,4 +1,4 @@
 export const SITUM_EMAIL = '';
 export const SITUM_API_KEY = '';
 export const SITUM_BUILDING_ID = ''; // Identifier of the building
-export const SITUM_FLOOR_ID = 0; // Identifier of the floor
+export const SITUM_FLOOR_ID = ''; // Identifier of the floor
