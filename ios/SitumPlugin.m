591f665ef32c733364813dc899c34470e3b53153
Cristian Mayán <cristian.mayan@situm.com>
2023-03-08 14:31:10 +0100

    Minor refactor os some iOS condition

--- ios/SitumPlugin.m
+++ ios/SitumPlugin.m
@@ -131,15 +131,15 @@ RCT_EXPORT_METHOD(setUserPass:(NSString *)email pass:(NSString *)pass withCallba
 }
 
 RCT_EXPORT_METHOD(setDashboardURL:(NSString *)url withCallback:(RCTResponseSenderBlock)callback) {
-    BOOL success = NO
+    BOOL success = NO;
     
-    if (url || url.length) {
-        success = YES
+    if (url && url.length) {
+        success = YES;
         [SITServices setDashboardURL:url];
     }
 
     if (callback) {
-        NSDictionary *response = @{@"success":success ? YES : NO};
+        NSDictionary *response = @{@"success":success ? @"true" : @"false"};
         callback(@[response]);
     }
 }
