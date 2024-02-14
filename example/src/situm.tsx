d9c425c98cef4137a7986c32e887c38d211d5ecd
Juan Antonio Martínez Mera <juan.martinez@situm.com>
2024-02-14 08:47:49 +0100

    fix(plugin): fix SDK computation issues

--- example/src/situm.tsx
+++ example/src/situm.tsx
@@ -1,4 +1,4 @@
-export const SITUM_API_KEY = '';
-export const SITUM_BUILDING_ID = ''; // Identifier of the building
-export const SITUM_FLOOR_ID = ''; // Identifier of the floor
+export const SITUM_API_KEY = '29f3a0e82f02a57e75b2665e65ed0f2cb4a856e7b4fd1d5ed67c68d6c86733ee';
+export const SITUM_BUILDING_ID = '15217'; // Identifier of the building
+export const SITUM_FLOOR_ID = '48943'; // Identifier of the floor
 export const SITUM_DASHBOARD_URL = 'https://dashboard.situm.com';
