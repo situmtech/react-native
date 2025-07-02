32dd511dc6219e656ae70f6e63d1bb5f3021a0f5
Cristian <cristian.mayan@situm.com>
2025-07-02 13:48:15 +0200

    feat: added 'persistOnUnmount' make MapView singleton to avoid re-rendering in StackNavigation integrations

--- plugin/src/wayfinding/index.ts
+++ plugin/src/wayfinding/index.ts
@@ -8,5 +8,8 @@ export { default as useSitum } from "./hooks";
 // State Provider
 export { default as SitumProvider } from "./store";
 
+// WebViewProvider
+export { WebViewProvider } from "./store/WebViewProvider";
+
 // Component
 export { default as MapView } from "./components/MapView";
