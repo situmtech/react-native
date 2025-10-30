8d191700b4838d4419e78088afab6d98e3ccdf4d
Cristian <cristian.mayan@situm.com>
2025-10-30 14:27:02 +0100

    chore: prevent other components from using our API before SitumProvider tries to initialize

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -16,13 +16,16 @@ import { SITUM_API_KEY, SITUM_API_DOMAIN } from "./situm";
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
-      {/** Make sure to authenticate with `SitumProvider.apiKey` at the root of your app */}
-      <SitumProvider apiKey={SITUM_API_KEY} apiDomain={SITUM_API_DOMAIN}>
-        <NavigationContainer>
+      <NavigationContainer>
+        {/**
+         * Make sure to authenticate with `SitumProvider.apiKey` before using SitumPlugin methods
+         * or rendering MapView component.
+         */}
+        <SitumProvider apiKey={SITUM_API_KEY} apiDomain={SITUM_API_DOMAIN}>
           <StatusBar barStyle="dark-content" />
           <TabNavigator />
-        </NavigationContainer>
-      </SitumProvider>
+        </SitumProvider>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
