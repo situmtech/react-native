04698885030edfc81a0ef9354baf3fc0b662b122
fsvilas <fernando.sanchez@situm.com>
2026-02-11 13:45:31 +0100

    fix: example icon

--- example/src/screens/cards/ShareLiveLocationCard.tsx
+++ example/src/screens/cards/ShareLiveLocationCard.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Button, View, StyleSheet, Platform, TextInput } from "react-native";
-import { Card, Icon, Text } from "react-native-paper";
+import { Card, Icon } from "react-native-paper";
 import { Colors, SharedStyles } from "../../SharedStyles";
 
-const CardTitleIcon = () => <Icon source="share-social-outline" size={20} />;
+const CardTitleIcon = () => <Icon source="share-variant" size={20} />;
 
 interface ShareLiveLocationCardProps {
   onSetShareLiveLocationSession: (identifier: string) => void;
