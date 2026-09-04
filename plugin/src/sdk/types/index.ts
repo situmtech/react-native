4ddef8f865ce6336f4fba9d63568571e5d8c026d
Rodrigo Lago <rodrigo.lago@situm.com>
2026-09-04 11:50:12 +0200

    Remove userDefinedThreshold

--- plugin/src/sdk/types/index.ts
+++ plugin/src/sdk/types/index.ts
@@ -468,7 +468,6 @@ export type DirectionsOptions = {
 export type OutdoorLocationOptions = {
   /** Environment detection continuous mode (true) or burst mode (false) */
   continuousMode?: boolean;
-  userDefinedThreshold?: boolean;
   /** Interval to scan for GPS and detect the environment (in seconds) */
   burstInterval?: number;
 };
