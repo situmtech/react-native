a086d2fecb71184bcbd9d66efa54eb087a440666
Adrián Pereiro <adrian.pereiro@situm.com>
2026-09-21 16:41:35 +0200

    Configure the Situm Maven repository through Expo build properties

--- plugin/app.plugin.js
+++ plugin/app.plugin.js
@@ -1,34 +1,16 @@
 // Expo config plugin that adds the Situm Maven repository to Android projects.
-const { WarningAggregator, withProjectBuildGradle } = require('expo/config-plugins')
+const { AndroidConfig } = require("expo/config-plugins");
 
-const SITUM_MAVEN_REPOSITORY = 'https://repo.situm.com/artifactory/libs-release-local'
-const SITUM_MAVEN_REPOSITORY_BLOCK = `
+const SITUM_MAVEN_REPOSITORY =
+  "https://repo.situm.com/artifactory/libs-release-local";
+const { createBuildGradlePropsConfigPlugin } = AndroidConfig.BuildProperties;
 
-allprojects {
-    repositories {
-        maven { url "${SITUM_MAVEN_REPOSITORY}" }
-    }
-}
-`;
-
-const withSitumMavenRepository = expoConfig =>
-  withProjectBuildGradle(expoConfig, gradleConfig => {
-    if (gradleConfig.modResults.language !== 'groovy') {
-      // Warn instead of adding Groovy syntax to an unsupported Gradle format.
-      WarningAggregator.addWarningAndroid(
-        'withSitumMavenRepository',
-        'Cannot automatically configure project build.gradle if it is not Groovy'
-      )
-
-      return gradleConfig
-    }
-
-    // Avoid adding the repository if it is already configured.
-    if (!gradleConfig.modResults.contents.includes(SITUM_MAVEN_REPOSITORY)) {
-      gradleConfig.modResults.contents += SITUM_MAVEN_REPOSITORY_BLOCK;
-    }
-
-    return gradleConfig
-  })
-
-module.exports = withSitumMavenRepository
+module.exports = createBuildGradlePropsConfigPlugin(
+  [
+    {
+      propName: "android.extraMavenRepos",
+      propValueGetter: () => JSON.stringify([{ url: SITUM_MAVEN_REPOSITORY }]),
+    },
+  ],
+  "withSitumMavenRepository",
+);
